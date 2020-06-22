dacc82368d9e10e66d719811f44d8e2cf888dc96
matt-r-mck <matt.r.mck@gmail.com>
2020-06-22 13:33:11 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PigDice {
+    class PigDice {
+
+        Random rnd = new Random();
+
+        int Roll() {
+            return rnd.Next(1, 7);
+        }
+
+        public int Play() {
+            var die = Roll();
+            var total = 0;
+            while (die != 1) {
+                total += die;
+                die = Roll();
+            }
+                return total;
+            }
+        }
+
+    }
---
+++ PigDice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PigDice {
+    class Program {
+        static void Main(string[] args) {
+
+            var pigdice = new PigDice();
+            var HighScore = 0;
+            for(var count = 1; count <= 10000000000; count++) {
+                var score =pigdice.Play();
+                if (score > HighScore) {
+                    HighScore = score;
+                }
+            }
+            Console.WriteLine($"The High Score is {HighScore}");
+        }
+    }
+}
